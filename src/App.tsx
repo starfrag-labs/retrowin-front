0f6b9f6ef1669527b1528df500dcef9701957535
sauce-git <sauce@mandacode.com>
2024-05-13 22:40:36 +0900

    refactor: Remove unused code and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,14 @@ import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { routeTree } from './routeTree.gen';
-import { useState } from 'react';
-import { useTokenStore } from './store/tokenStore';
 
+const queryClient = new QueryClient();
 const router = createRouter({
   routeTree,
   context: {
-    accessToken: '',
-    setAccessToken: () => {},
-    theme: 'light',
-    setTheme: () => {},
+    queryClient: queryClient,
   },
 });
-const queryClient = new QueryClient();
 
 declare module '@tanstack/react-router' {
   interface Register {
@@ -23,15 +18,10 @@ declare module '@tanstack/react-router' {
 }
 
 export function App() {
-  const accessToken = useTokenStore((state) => state.accessToken);
-  const setAccessToken = useTokenStore((state) => state.updateAccessToken);
-  const [theme, setTheme] = useState('light');
-
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider
         router={router}
-        context={{ accessToken, setAccessToken, theme: theme, setTheme }}
       />
       <ReactQueryDevtools
         initialIsOpen={false}
