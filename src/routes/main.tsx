674f88d8c63710c861c0e7ed074f7aaa8d69ffb4
sauce-git <sauce@mandacode.com>
2024-06-29 13:54:35 +0900

    chore: Remove console.log statement in MainComponent

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -123,8 +123,6 @@ function MainComponent() {
                 <Elements folderKey={rootFolderKey} />
               </div>
               {window.map((window) => {
-                console.log(window);
-                
                 return <WindowV2 key={window.key} windowKey={window.key} />;
               })}
             </OptionMenu>
