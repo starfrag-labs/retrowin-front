ed10dcf1c5def1b008eebf66a999ee227a983987
sauce-git <sauce@mandacode.com>
2024-07-13 16:24:19 +0900

    refactor: Remove console.log statement in index component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,7 +17,6 @@ export const Route = createFileRoute('/')({
     await getProfile()
       .then((response) => {
         setProfile(response.data.data);
-        console.log(response.data.data);
       })
       .catch(() => {
         window.location.href = `${config.auth}?redirect=${config.redirectUrl}`;
