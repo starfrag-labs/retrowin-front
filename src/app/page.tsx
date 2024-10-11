dbefbc217eab63fc6786fdb3e31ec0d17fd9ee26
sauce-git <sauce@mandacode.com>
2024-10-11 19:43:00 +0900

    feat: add downloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Uploader from "@/components/uploader";
 import styles from "./page.module.css";
+import Downloader from "@/components/downloader";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Uploader />
+      <Downloader />
     </div>
   );
 }
