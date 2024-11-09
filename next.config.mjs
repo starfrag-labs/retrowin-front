494dab9bfc5341c4579dc96994aaa2bef54e0af6
sauce-git <sauce@mandacode.com>
2024-11-09 21:59:25 +0900

    fix: fix image src prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
+    path: process.env.ASSET_PREFIX,
     remotePatterns: [
       {
         hostname: "ifcloud-storage",
-        port: "3001",
+        port: "3000",
         //hostname: "127.0.0.1",
         //port: "3002",
       }
     ],
   },
-  output: "standalone",
   assetPrefix: process.env.ASSET_PREFIX,
 };
 
--- src/components/layout/background.tsx
+++ src/components/layout/background.tsx
@@ -4,11 +4,10 @@ import styles from "./background.module.css";
 export default function Background({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const prefix = process.env.NEXT_PUBLIC_APP_PREFIX ?? "";
   return (
     <div className={styles.container}>
       <Image
-        src={`${prefix}/assets/background.webp`}
+        src={"/assets/background.webp"}
         alt="background"
         fill
         className={styles.image}
