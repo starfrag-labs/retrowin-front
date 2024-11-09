d8f4b0e03cfd0d90ea71fcf7a0fd86755d796155
sauce-git <sauce@mandacode.com>
2024-11-09 22:55:56 +0900

    fix: fix image path

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
   output: "standalone",
   images: {
-    path: process.env.ASSET_PREFIX,
+    path: `${process.env.ASSET_PREFIX}/_next/image`,
     remotePatterns: [
       {
         hostname: "ifcloud-storage",
         port: "3000",
         //hostname: "127.0.0.1",
         //port: "3002",
-      }
+      },
     ],
   },
   assetPrefix: process.env.ASSET_PREFIX,
