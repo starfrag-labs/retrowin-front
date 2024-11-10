b139b2420a867a244b03fed5a8c7b47132b44c6c
sauce-git <sauce@mandacode.com>
2024-11-10 16:16:54 +0900

    fix: fix query returned data handling

--- src/components/menu/window_menu.tsx
+++ src/components/menu/window_menu.tsx
@@ -53,15 +53,17 @@ export default function WindowMenu({
     const files = await queryClient.fetchQuery(
       fileQuery.read.children(targetFileKey),
     );
-    Promise.all(
-      files.data.map((file) =>
-        deleteFilePermanentMutation.mutateAsync({ fileKey: file.fileKey }),
-      ),
-    ).then(() => {
-      queryClient.invalidateQueries({
-        queryKey: ["file", targetFileKey],
+    if (files) {
+      Promise.all(
+        files.data.map((file) =>
+          deleteFilePermanentMutation.mutateAsync({ fileKey: file.fileKey }),
+        ),
+      ).then(() => {
+        queryClient.invalidateQueries({
+          queryKey: ["file", targetFileKey],
+        });
       });
-    });
+    }
   }, [closeMenu, deleteFilePermanentMutation, queryClient, targetFileKey]);
 
   const handleRefresh = useCallback(async () => {
