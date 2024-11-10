2a11ca82a76c7bd0e739e730bcda05d4bbf37c93
sauce-git <sauce@mandacode.com>
2024-11-11 00:53:21 +0900

    feat: uploader and window design update

--- src/components/window/window_content/uploader.tsx
+++ src/components/window/window_content/uploader.tsx
@@ -1,6 +1,7 @@
 import styles from "./uploader.module.css";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { fileQuery, storageQuery } from "@/api/query";
+import { useState } from "react";
 
 export default function Uploader({
   targetContainerKey,
@@ -9,13 +10,44 @@ export default function Uploader({
 }) {
   const chunkSize = 1024 * 1024; // 1MB
 
+  // State
+  const [uploadState, setUploadState] = useState<
+    {
+      fileKey: string;
+      fileName: string;
+      totalSize: number;
+      uploadedSize: number;
+    }[]
+  >([]);
+  // Upload state actions
+  const addUpload = (upload: {
+    fileKey: string;
+    fileName: string;
+    totalSize: number;
+    uploadedSize: number;
+  }) => {
+    setUploadState((state) => [...state, upload]);
+  };
+  const updateUpload = (fileKey: string, uploadedSize: number) => {
+    setUploadState((state) =>
+      state.map((upload) =>
+        upload.fileKey === fileKey ? { ...upload, uploadedSize } : upload,
+      ),
+    );
+  };
+  const removeUpload = (fileKey: string) => {
+    setUploadState((state) =>
+      state.filter((upload) => upload.fileKey !== fileKey),
+    );
+  };
+
   // Query Client
   const queryClient = useQueryClient();
 
   // Mutations
-  const issueWriteSessionQuery = useMutation(storageQuery.session.write);
-  const writeFileQuery = useMutation(storageQuery.file.write);
-  const completeUploadQuery = useMutation(fileQuery.upload.complete);
+  const issueWriteSessionMutation = useMutation(storageQuery.session.write);
+  const writeFileMutation = useMutation(storageQuery.file.write);
+  const completeUploadMutation = useMutation(fileQuery.upload.complete);
 
   const uploadFiles = async (files: FileList) => {
     // Sort files by size
@@ -36,32 +68,56 @@ export default function Uploader({
     const totalChunks = Math.ceil(file.size / chunkSize);
 
     // Issue write session
-    const response = await issueWriteSessionQuery.mutateAsync({
-      targetContainerKey,
-      fileName: file.name,
-      size: file.size,
-    });
-    const fileKey = response.fileKey;
-
-    // Upload chunks
-    for (let i = 0; i < totalChunks; i++) {
-      // Calculate chunk start and end
-      const start = i * chunkSize;
-      const end = Math.min(file.size, start + chunkSize);
-      const chunk = file.slice(start, end);
-      // Write chunk to storage
-      await writeFileQuery.mutateAsync({
-        fileKey,
-        chunkCount: i,
-        fileData: chunk,
+    const fileKey = await issueWriteSessionMutation
+      .mutateAsync({
+        targetContainerKey,
+        fileName: file.name,
+        size: file.size,
+      })
+      .then((response) => {
+        const fileKey = response.fileKey;
+        if (fileKey) {
+          addUpload({
+            fileKey: fileKey,
+            fileName: file.name,
+            totalSize: file.size,
+            uploadedSize: 0,
+          });
+        }
+        return fileKey;
       });
-    }
 
-    // Complete upload
-    await completeUploadQuery.mutateAsync({
-      fileKey,
-      totalChunks,
-    });
+    if (fileKey) {
+      // Upload chunks
+      for (let i = 0; i < totalChunks; i++) {
+        // Calculate chunk start and end
+        const start = i * chunkSize;
+        const end = Math.min(file.size, start + chunkSize);
+        const chunk = file.slice(start, end);
+        // Write chunk to storage
+        await writeFileMutation
+          .mutateAsync({
+            fileKey,
+            chunkCount: i,
+            fileData: chunk,
+          })
+          .then(() => {
+            // Update upload state
+            updateUpload(fileKey, end);
+          })
+      }
+
+      // Complete upload
+      await completeUploadMutation
+        .mutateAsync({
+          fileKey,
+          totalChunks,
+        })
+        .finally(() => {
+          // Remove upload state
+          removeUpload(fileKey);
+        });
+    }
   };
 
   const handleUpload = (e: React.FormEvent<HTMLFormElement>) => {
@@ -74,7 +130,23 @@ export default function Uploader({
   };
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} full-size`}>
+      <div className={`full-size`}>
+        <div className={`${styles.upload_state} full-size`}>
+          {uploadState.map((upload) => (
+            <div key={upload.fileKey} className={styles.progress_container}>
+              <span>{upload.fileName}</span>
+              <progress
+                value={upload.uploadedSize}
+                max={upload.totalSize}
+              ></progress>
+              <div>
+                {Math.round((upload.uploadedSize / upload.totalSize) * 100)}%
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
       <form className={styles.form} onSubmit={handleUpload}>
         <input type="file" className={styles.input} multiple />
         <button type="submit" className={styles.button}>
